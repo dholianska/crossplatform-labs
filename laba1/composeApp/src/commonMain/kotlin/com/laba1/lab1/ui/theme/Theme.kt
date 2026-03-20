e92c72c9fdcb1139d1c35d45046ede4298f567b5
daria <dholianska26@gmail.com>
2026-03-20 11:10:05 +0200

    [lab2] added sample of the logic usage

---
+++ laba1/composeApp/src/commonMain/kotlin/com/laba1/lab1/ui/theme/Theme.kt
@@ -0,0 +1,15 @@
+package com.laba1.lab1.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AppTheme(content: @Composable () -> Unit) {
+    val colors = lightColorScheme()
+
+    MaterialTheme(
+        colorScheme = colors,
+        content = content
+    )
+}
